334c4efd8df1395339878f50b6ce8ec6532fbd22
Blitu82 <garpaues@yahoo.es>
2024-03-20 20:54:58 +0000

    updated env

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 // const baseURL = `${import.meta.env.BACKEND_URL}/auth`;
 // console.log(import.meta.env.BACKEND_URL);
 
-const baseURL = 'http://localhost:5005/auth';
+// const baseURL = 'http://localhost:5005/auth';
+const baseURL = `${import.meta.env.VITE_BACKEND_URL}/auth`;
 
 export const signup = user => {
   return axios.post(`${baseURL}/signup`, user);
--- src/api/feedback.api.js
+++ src/api/feedback.api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:5005/api';
+// const baseURL = 'http://localhost:5005/api';
+
+const baseURL = `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 export const feedback = feedbackData => {
   const storedToken = localStorage.getItem('authToken');
